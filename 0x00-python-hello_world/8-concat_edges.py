89372deb5610f558ca3fe1d4c9c545196754b64e
YassineHaddad23 <metacodefreelancer@gmail.com>
2024-07-28 04:59:13 -0700

    Initial commit with all required files

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "object-oriented programming with Python"
+str = str[0:6] + str[7:8] + str[10] + str[13:24]
+print(str)
---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,3 @@
+# alx-higher_level_programming
+
+This repository contains projects for the ALX Higher Level Programming track.
